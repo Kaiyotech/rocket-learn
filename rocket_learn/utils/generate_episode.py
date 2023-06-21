e1b70d5ed8ff0bd0b34d4a3c64feb904563e90f9
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2023-06-21 10:12:49 -0400

    fix bug in generate_episode.py that allowed a quick switch back because force_selector was used to choice do_selector before the parser looked at the new action to set force_selector.

--- rocket_learn/utils/generate_episode.py
+++ rocket_learn/utils/generate_episode.py
@@ -8,14 +8,14 @@ from rlgym.utils.state_setters import DefaultState
 from rlgym.utils.terminal_conditions.common_conditions import GoalScoredCondition
 from tqdm import tqdm
 
-
 from rocket_learn.agent.policy import Policy
 from rocket_learn.agent.pretrained_policy import HardcodedAgent
 from rocket_learn.experience_buffer import ExperienceBuffer
 from rocket_learn.utils.dynamic_gamemode_setter import DynamicGMSetter
 
 
-def generate_episode(env: Gym, policies, versions, eval_setter=DefaultState(), evaluate=False, scoreboard=None, progress=False, selector_skip_k=None,
+def generate_episode(env: Gym, policies, versions, eval_setter=DefaultState(), evaluate=False, scoreboard=None,
+                     progress=False, selector_skip_k=None,
                      force_selector_choice=None,
                      unlock_selector_indices=None,
                      unlock_indices_group=None,
@@ -169,70 +169,27 @@ def generate_episode(env: Gym, policies, versions, eval_setter=DefaultState(), e
                 else:
                     all_actions[idx] = last_actions[idx]
                 pretrained_idx_tss_dict[idx][1] = (
-                    pretrained_idx_tss_dict[idx][1] + 1) % pretrained_idx_tss_dict[idx][0]
+                                                          pretrained_idx_tss_dict[idx][1] + 1) % \
+                                                  pretrained_idx_tss_dict[idx][0]
 
-            # The below code can be removed
-            # if not isinstance(policies[0], HardcodedAgent) and all(policy == policies[0] for policy in policies):
-            #     policy = policies[0]
-            #     if isinstance(observations[0], tuple):
-            #         obs = tuple(np.concatenate([obs[i] for obs in observations], axis=0)
-            #                     for i in range(len(observations[0])))
-            #     else:
-            #         obs = np.concatenate(observations, axis=0)
-            #     dist = policy.get_action_distribution(obs)
-            #     action_indices = policy.sample_action(dist)
-            #     log_probs = policy.log_prob(dist, action_indices)
+            # to allow different action spaces, pad out short ones to longest length (assume later unpadding in parser)
+            length = max([a.shape[0] for a in all_actions])
+            padded_actions = []
+            for a in all_actions:
+                action = np.pad(
+                    a.astype('float64'), (0, length - a.size), 'constant', constant_values=np.NAN)
+                padded_actions.append(action)
 
-            #     if do_selector[0]:
-            #         actions = policy.env_compatible(action_indices)
-            #         last_actions = actions
+            all_actions = padded_actions
+            # TEST OUT ABOVE TO DEAL WITH VARIABLE LENGTH
 
-            #     else:
-            #         actions = last_actions
+            all_actions = np.vstack(all_actions)
+            old_obs = observations
+            observations, rewards, done, info = env.step(all_actions)
+            if len(policies) <= 1:
+                observations, rewards = [observations], [rewards]
 
-            #     all_indices.extend(list(action_indices.numpy()))
-            #     all_actions.extend(list(actions))
-            #     all_log_probs.extend(list(log_probs.numpy()))
-            # else:
-            #     index = 0
-            #     for policy, obs in zip(policies, observations):
-            #         if isinstance(policy, HardcodedAgent):
-            #             actions = policy.act(last_state, index)
-
-            #             # make sure output is in correct format
-            #             if not isinstance(observations, np.ndarray):
-            #                 actions = np.array(actions)
-
-            #             # TODO: add converter that takes normal 8 actions into action space
-            #             # actions = env._match._action_parser.convert_to_action_space(actions)
-
-            #             all_indices.append(None)
-            #             all_actions.append(actions)
-            #             all_log_probs.append(None)
-
-            #         elif isinstance(policy, Policy):
-            #             dist = policy.get_action_distribution(obs)
-            #             action_indices = policy.sample_action(dist)[0]
-            #             log_probs = policy.log_prob(
-            #                 dist, action_indices).item()
-
-            #             if do_selector[index]:
-            #                 actions = policy.env_compatible(action_indices)
-            #                 last_actions[index] = actions
-
-            #             else:
-            #                 actions = last_actions[index]
-
-            #             all_indices.append(action_indices.numpy())
-            #             all_actions.append(actions)
-            #             all_log_probs.append(log_probs)
-
-            #         else:
-            #             print(str(type(policy)) + " type use not defined")
-            #             assert False
-
-            #         index += 1
-
+            # need to check the force and stuff AFTER the parser has a look at the new action choice
             if selector_skip_k is not None:
                 for i in range(len(do_selector)):
                     if not isinstance(policies[i], HardcodedAgent):
@@ -252,23 +209,6 @@ def generate_episode(env: Gym, policies, versions, eval_setter=DefaultState(), e
             for i in range(len(tick)):
                 tick[i] = 0 if do_selector[i] else tick[i] + 1
                 boost_tick[i] = 0 if do_boost[i] else boost_tick[i] + 1
-
-            # to allow different action spaces, pad out short ones to longest length (assume later unpadding in parser)
-            length = max([a.shape[0] for a in all_actions])
-            padded_actions = []
-            for a in all_actions:
-                action = np.pad(
-                    a.astype('float64'), (0, length - a.size), 'constant', constant_values=np.NAN)
-                padded_actions.append(action)
-
-            all_actions = padded_actions
-            # TEST OUT ABOVE TO DEAL WITH VARIABLE LENGTH
-
-            all_actions = np.vstack(all_actions)
-            old_obs = observations
-            observations, rewards, done, info = env.step(all_actions)
-            if len(policies) <= 1:
-                observations, rewards = [observations], [rewards]
 
             # prune data that belongs to old agents
             old_obs = [a for i, a in enumerate(
