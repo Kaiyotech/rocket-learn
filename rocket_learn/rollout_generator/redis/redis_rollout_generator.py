f37618f253ed0e407ac7f42896ce78261dcc50e1
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2024-03-04 11:18:44 -0500

    change reward stage step

--- rocket_learn/rollout_generator/redis/redis_rollout_generator.py
+++ rocket_learn/rollout_generator/redis/redis_rollout_generator.py
@@ -455,9 +455,9 @@ class RedisRolloutGenerator(BaseRolloutGenerator):
                 print("redis bgsave failed, auto save already in progress")
 
     def set_reward_stage(self, total_timesteps):
-        if total_timesteps >= 3_000_000_000:
+        if total_timesteps >= 5_000_000_000:
             self.redis.set(REWARD_STAGE, 3)
-        elif total_timesteps >= 1_500_000_000:
+        elif total_timesteps >= 2_750_000_000:
             self.redis.set(REWARD_STAGE, 2)
         elif total_timesteps >= 450_000_000:
             self.redis.set(REWARD_STAGE, 1)
